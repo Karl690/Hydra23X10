03a9b183739c774ba94c8686d14ed6a7551e4e94
falcon developer <falcon198236@outlook.com>
2023-09-01 15:50:22 +0200

    Original project 9/1/2023

---
+++ HydraX10VG/HydraX10VG/inc/pnp.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h> 
+extern uint32_t PnPFeederValue;
+extern uint16_t PnPResetTimer;//spi3 valve timer dwell time
+extern uint16_t PNPSPIData;//spi3 valve control data word;
+
+void LatchPnPData();
+void PnP_SetValves();
+void PnP_TurnOffAllValves();
+void SendPNPSPIDataToSpi3(uint16_t DataToSend);
+
