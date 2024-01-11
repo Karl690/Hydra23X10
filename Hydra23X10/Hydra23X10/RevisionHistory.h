004350c0306688207ace5b1a178afa7ee5210ac5
falcon developer <falcon198236@outlook.com>
2024-01-11 21:55:59 +0100

    1/11/2024 -commit-2

--- Hydra23X10/Hydra23X10/RevisionHistory.h
+++ Hydra23X10/Hydra23X10/RevisionHistory.h
@@ -2,5 +2,5 @@
 #define RevisionHeader "HYDRA23X10"
 #define MajorStep "4.253a"
 #define RevisionDate "1/11/2024"
-#define InstallPath "c:\\repetrel\\bin\\firmware\\"
+#define InstallPath "c:/repetrel/bin/firmware/"
 //#define CONNECTIONSTRING ">GE:MEG Rev " MajorStep ":" RevisionDate ":STM32F407:---:"   //"1.000:0:0:0:USB"
