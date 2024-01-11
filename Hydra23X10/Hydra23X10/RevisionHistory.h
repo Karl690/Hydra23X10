3fc10c86fa836903e7ca80154600431e9fb2c89a
falcon developer <falcon198236@outlook.com>
2024-01-11 18:47:57 +0100

    4.253a 1/11/2024 added dfulauncher.

---
+++ Hydra23X10/Hydra23X10/RevisionHistory.h
@@ -0,0 +1,6 @@
+#pragma once
+#define RevisionHeader "HYDRA23X10"
+#define MajorStep "4.253a"
+#define RevisionDate "1/11/2024"
+#define InstallPath "c:\\repetrel\\bin\\firmware\\"
+//#define CONNECTIONSTRING ">GE:MEG Rev " MajorStep ":" RevisionDate ":STM32F407:---:"   //"1.000:0:0:0:USB"
